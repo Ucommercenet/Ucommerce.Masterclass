229a7a14808b98588e51f9bdf544334e7ae01726
Skjoldager <msk@ucommerce.net>
2020-08-13 10:29:48 +0200

    Upload solution.

---
+++ src/Ucommerce.Masterclass.Umbraco/IndexDefinitions/MasterclassIndexDefinition.cs
@@ -0,0 +1,13 @@
+using Ucommerce.Search.Definitions;
+using Ucommerce.Search.Extensions;
+
+namespace Ucommerce.Masterclass.Umbraco.IndexDefinitions
+{
+    public class MasterclassIndexDefinition : DefaultProductsIndexDefinition
+    {
+        public MasterclassIndexDefinition() : base()
+        {
+            
+        }
+    }
+}

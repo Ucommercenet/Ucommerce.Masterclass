9f5a4d58b22001c3803f02781c4b1f6f21a329ea
Morten Skjoldager <msk@ucommerce.net>
2020-08-20 16:21:03 +0200

    Implemented facets.

--- src/Ucommerce.Masterclass.Umbraco/Controllers/CategoryController.cs
+++ src/Ucommerce.Masterclass.Umbraco/Controllers/CategoryController.cs
@@ -1,17 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using Ucommerce.Api;
 using Ucommerce.Infrastructure;
 using Ucommerce.Masterclass.Umbraco.Models;
+using Ucommerce.Search.Extensions;
 using Ucommerce.Search.Facets;
 using Ucommerce.Search.Models;
 using Ucommerce.Search.Slugs;
+using Umbraco.Core;
 using Umbraco.Web.Mvc;
 
 namespace Ucommerce.Masterclass.Umbraco.Controllers
 {
+    public static class FacetedQueryStringExtensions
+    {
+        public static IList<Facet> ToFacets(this NameValueCollection target)
+        {
+            var parameters = new Dictionary<string, string>();
+            foreach (var queryString in HttpContext.Current.Request.QueryString.AllKeys)
+            {
+                parameters[queryString] = HttpContext.Current.Request.QueryString[queryString];
+            }
+
+            parameters.RemoveAll(kvp =>
+                new [] { "umbDebugShowTrace", "product", "variant", "category", "categories", "catalog"}
+                    .Contains(kvp.Key));
+
+            var facetsForQuerying = new List<Facet>();
+
+            foreach (var parameter in parameters)
+            {
+                var facet = new Facet {FacetValues = new List<FacetValue>(), Name = parameter.Key};
+                foreach (var value in parameter.Value.Split(new[] {'|'}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    facet.FacetValues.Add(new FacetValue() {Value = value});
+                }
+
+                facetsForQuerying.Add(facet);
+            }
+
+            return facetsForQuerying;
+        }
+    }
+
     public class CategoryController : RenderMvcController
     {
         public ICatalogLibrary CatalogLibrary => ObjectFactory.Instance.Resolve<ICatalogLibrary>();
@@ -41,7 +76,9 @@ namespace Ucommerce.Masterclass.Umbraco.Controllers
             categoryModel.Name = currentCategory.Name;
             categoryModel.ImageMediaUrl = currentCategory.ImageMediaUrl;
 
-            var facetResultSet = CatalogLibrary.GetProducts(currentCategory.Guid, new FacetDictionary());
+            var facetDictionary = System.Web.HttpContext.Current.Request.QueryString.ToFacets().ToFacetDictionary();
+
+            var facetResultSet = CatalogLibrary.GetProducts(currentCategory.Guid, facetDictionary);
             
             categoryModel.Facets = MapFacets(facetResultSet.Facets);
             categoryModel.TotalProductsCount = facetResultSet.TotalCount;

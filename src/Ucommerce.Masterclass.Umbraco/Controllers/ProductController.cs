49bd59c4eee500d305a356aac3e2da6e2d171c44
Morten Skjoldager <msk@ucommerce.net>
2020-08-25 16:11:36 +0200

    variants picker.

--- src/Ucommerce.Masterclass.Umbraco/Controllers/ProductController.cs
+++ src/Ucommerce.Masterclass.Umbraco/Controllers/ProductController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Ucommerce.Api;
 using Ucommerce.Infrastructure;
 using Ucommerce.Masterclass.Umbraco.Models;
+using Ucommerce.Search;
+using Ucommerce.Search.Models;
 using Umbraco.Web.Mvc;
 
 namespace Ucommerce.Masterclass.Umbraco.Controllers
@@ -24,9 +27,18 @@ namespace Ucommerce.Masterclass.Umbraco.Controllers
             productModel.Name = currentProduct.DisplayName;
 
             productModel.Prices = CatalogLibrary.CalculatePrices(new List<Guid>() {currentProduct.Guid}).Items;
-            
-            
+            productModel.Variants = MapVariants(CatalogLibrary.GetVariants(currentProduct));            
             return View(productModel);
+        }
+
+        private IList<ProductViewModel> MapVariants(ResultSet<Product> variants)
+        {
+           return variants.Select(x =>
+                new ProductViewModel
+                {
+                    Name = x.DisplayName,
+                    VariantSku = x.VariantSku
+                }).ToList();
         }
     }
 }

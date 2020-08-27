e2529ca5696225394cb487daca187e11ac1843e0
Morten Skjoldager <msk@ucommerce.net>
2020-08-27 11:40:54 +0200

    View for billing controller.

--- src/Ucommerce.Masterclass.Umbraco/Controllers/BillingController.cs
+++ src/Ucommerce.Masterclass.Umbraco/Controllers/BillingController.cs
@@ -1,7 +1,18 @@
+using System.Web.Mvc;
+using Ucommerce.Api;
+using Ucommerce.Infrastructure;
+using Umbraco.Web.Mvc;
+
 namespace Ucommerce.Masterclass.Umbraco.Controllers
 {
-    public class BillingController
+    public class BillingController : RenderMvcController
     {
-        
+        public ITransactionLibrary TransactionLibrary => ObjectFactory.Instance.Resolve<ITransactionLibrary>();
+
+        [System.Web.Mvc.HttpGet]
+        public ActionResult Index()
+        {
+            return Index();
+        }
     }
 }

d6639e8c4168df5eb589374e3363dea6543d94a2
Morten Skjoldager <msk@ucommerce.net>
2020-09-01 10:40:43 +0200

    Added controlelrs and views for widgets for master class.

---
+++ src/Ucommerce.Masterclass.Sitefinity/Mvc/Controllers/BasketController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+using Ucommerce.Api;
+using Ucommerce.EntitiesV2;
+using Ucommerce.Infrastructure;
+using Ucommerce.Marketing;
+using Ucommerce.Marketing.Awards.AwardResolvers;
+using Ucommerce.Marketing.TargetingContextAggregators;
+using Ucommerce.Marketing.Targets.TargetResolvers;
+using Ucommerce.Masterclass.Sitefinity.Mvc.Models;
+
+namespace Ucommerce.Masterclass.Sitefinity.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "Basket", Title = "Basket", SectionName = "MasterClass")]
+    public class BasketController : Controller
+    {
+        public ITransactionLibrary TransactionLibrary => ObjectFactory.Instance.Resolve<ITransactionLibrary>();
+
+        [System.Web.Mvc.HttpGet]
+        public ActionResult Index()
+        {
+            var basket = TransactionLibrary.GetBasket(false);
+
+            return View(new PurchaseOrderViewModel()
+            {
+                TaxTotal = new Money(basket.TaxTotal.GetValueOrDefault(), basket.BillingCurrency.ISOCode).ToString(),
+                OrderTotal = new Money(basket.OrderTotal.GetValueOrDefault(), basket.BillingCurrency.ISOCode).ToString(),
+                ShippingTotal = new Money(basket.ShippingTotal.GetValueOrDefault(), basket.BillingCurrency.ISOCode).ToString(),
+                SubTotal = new Money(basket.SubTotal.GetValueOrDefault(), basket.BillingCurrency.ISOCode).ToString(),
+                OrderLines = basket.OrderLines.Select(x => new OrderlineViewModel()
+                {
+                    Quantity = x.Quantity,
+                    ProductName = x.ProductName,
+                    OrderLineId = x.OrderLineId,
+                    Sku = x.Sku,
+                    Total = new Money(x.Total.GetValueOrDefault(), basket.BillingCurrency.ISOCode).ToString(),
+                    UnitPrice = new Money(x.Price, basket.BillingCurrency.ISOCode).ToString(),
+                    Tax = new Money(x.VAT, basket.BillingCurrency.ISOCode).ToString(),
+                }).ToList()
+            });
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Index(int quantity, int orderlineId)
+        {
+            TransactionLibrary.UpdateLineItemByOrderLineId(orderlineId, quantity);
+            TransactionLibrary.ExecuteBasketPipeline();
+            
+            return Index();
+        }
+    }
+}

db66fa310747cc03c86870371b4d6e1a8391b394
Skjoldager <msk@ucommerce.net>
2021-11-18 13:13:54 +0100

    Added composer so you can inject directly into controllers instead of resolving (explicit) via ObjectFactory.

--- src/Ucommerce.Masterclass.Umbraco/Composers/UcommerceComposer.cs
+++ src/Ucommerce.Masterclass.Umbraco/Composers/UcommerceComposer.cs
@@ -8,6 +8,8 @@ namespace Ucommerce.Masterclass.Umbraco.Composers
     {
         public void Compose(Composition composition)
         {
+            //Always use transient lifetime scope as this is what the IOC container of Ucommerce is tailored for. If you register as singleton, you can get unforseen sideeffects.
+            
             //Services for browse
             composition.RegisterFor<Ucommerce.Api.ICatalogLibrary, Ucommerce.Api.CatalogLibrary>(x => Ucommerce.Infrastructure.ObjectFactory.Instance.Resolve<ICatalogLibrary>());
             composition.RegisterFor<Ucommerce.Api.ICatalogContext, Ucommerce.Api.CatalogContext>(x => Ucommerce.Infrastructure.ObjectFactory.Instance.Resolve<ICatalogContext>());

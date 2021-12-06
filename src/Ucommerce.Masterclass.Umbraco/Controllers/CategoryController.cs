7a9ff25a9a381b20be2dcebb05b48ae199c8b517
Mads Due <mdu@ucommerce.net>
2021-12-06 09:15:40 +0100

    moved extensions method into its own class

--- src/Ucommerce.Masterclass.Umbraco/Controllers/CategoryController.cs
+++ src/Ucommerce.Masterclass.Umbraco/Controllers/CategoryController.cs
@@ -1,52 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Ucommerce.Api;
 using Ucommerce.Infrastructure;
+using Ucommerce.Masterclass.Umbraco.Extensions;
 using Ucommerce.Masterclass.Umbraco.Models;
 using Ucommerce.Search.Extensions;
 using Ucommerce.Search.Facets;
 using Ucommerce.Search.Models;
 using Ucommerce.Search.Slugs;
-using Umbraco.Core;
 using Umbraco.Web.Mvc;
 
 namespace Ucommerce.Masterclass.Umbraco.Controllers
 {
-    public static class FacetedQueryStringExtensions
-    {
-        public static IList<Facet> ToFacets(this NameValueCollection target)
-        {
-            var parameters = new Dictionary<string, string>();
-            foreach (var queryString in HttpContext.Current.Request.QueryString.AllKeys)
-            {
-                parameters[queryString] = HttpContext.Current.Request.QueryString[queryString];
-            }
-
-            parameters.RemoveAll(kvp =>
-                new[] { "umbDebugShowTrace", "product", "variant", "category", "categories", "catalog" }
-                    .Contains(kvp.Key));
-
-            var facetsForQuerying = new List<Facet>();
-
-            foreach (var parameter in parameters)
-            {
-                var facet = new Facet { FacetValues = new List<FacetValue>(), Name = parameter.Key };
-                foreach (var value in parameter.Value.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    facet.FacetValues.Add(new FacetValue() { Value = value });
-                }
-
-                facetsForQuerying.Add(facet);
-            }
-
-            return facetsForQuerying;
-        }
-    }
-
     public class CategoryController : RenderMvcController
     {
         public ICatalogLibrary CatalogLibrary => ObjectFactory.Instance.Resolve<ICatalogLibrary>();
@@ -56,11 +22,6 @@ namespace Ucommerce.Masterclass.Umbraco.Controllers
         public ITransactionLibrary TransactionLibrary => ObjectFactory.Instance.Resolve<ITransactionLibrary>();
 
         public IUrlService UrlService => ObjectFactory.Instance.Resolve<IUrlService>();
-
-        public CategoryController()
-        {
-
-        }
 
         [System.Web.Mvc.HttpPost]
         public ActionResult Index(string sku)

914039b8c6955957b972fe284caa2ec8afcf4519
Morten Skjoldager <msk@ucommerce.net>
2021-12-20 15:58:54 +0100

    added partil view for login

---
+++ src/Ucommerce.Masterclass.Umbraco/Controllers/LoginNavigationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Ucommerce.Masterclass.Umbraco.Controllers
+{
+    public class LoginNavigationController : SurfaceController
+    {
+        public ActionResult LoginNavigation()
+        {
+            return View("/views/CategoryNavigation/index.cshtml");
+        }
+    }
+}

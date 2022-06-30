2fcea7fef510271d9d73b9e7df2aff76025d8942
Daniel Berg Frederiksen <dbf@ucommerce.dk>
2022-06-30 09:09:30 +0200

    Update ProductController.cs

--- src/Ucommerce.Masterclass.Umbraco/Controllers/ProductController.cs
+++ src/Ucommerce.Masterclass.Umbraco/Controllers/ProductController.cs
@@ -5,7 +5,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using Ucommerce.Masterclass.Umbraco.Exceptions;
 using Ucommerce.Masterclass.Umbraco.Headless;
 using Ucommerce.Masterclass.Umbraco.Resolvers;
 using Ucommerce.Api;
